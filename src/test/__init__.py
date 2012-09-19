c31cbaee23b7042775fd818dbc4619e69a9c7cae
nicfit <travis@pobox.com>
2012-09-18 22:33:55 -0600

    Command line script and test beginnings

---
+++ src/test/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#  Copyright (C) 2010-2012  Travis Shirk <travis@pobox.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+################################################################################
+import os, logging, sys
+import eyed3
+
+DATA_D = os.path.join(os.path.abspath(os.path.curdir), "src", "test", "data")
+
+eyed3.log.setLevel(logging.ERROR)
+
+
+class RedirectStdStreams(object):
+    '''FIXME : what is this for?'''
+    def __init__(self, stdout=None, stderr=None):
+        self._stdout = stdout or sys.stdout
+        self._stderr = stderr or sys.stderr
+
+    def __enter__(self):
+        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
+        self.old_stdout.flush()
+        self.old_stderr.flush()
+        sys.stdout, sys.stderr = self._stdout, self._stderr
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._stdout.flush()
+        self._stderr.flush()
+        sys.stdout = self.old_stdout
+        sys.stderr = self.old_stderr
