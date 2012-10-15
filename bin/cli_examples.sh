791c6cdfbe3d9f7003d8946079f6e624f0617fe7
nicfit <travis@pobox.com>
2012-10-15 07:04:24 -0600

    uncog

--- bin/cli_examples.sh
+++ bin/cli_examples.sh
@@ -12,17 +12,25 @@ eyeD3 --artist="Token Entry" --title="Entities" example.mp3
 
 # [[[section ALB_YR_SET]]]
 eyeD3 -A "Jaybird" -Y 1987 example.mp3
+eyeD3 -G "Hardcore" example.mp3
 # [[[endsection]]]
 
 
 # [[[section CLEAR_SET]]]
-eyeD3 --artist="" --title="" --album="" --genre="" --release-year="" example.mp3
+eyeD3 --genre="" example.mp3
 # [[[endsection]]]
 
 # [[[section ALL]]]
+# Set an artist value in the ID3 v1 tag
 eyeD3 -1 example.mp3 -a id3v1
+# The file now has a v1 and v2 tag, change the v2 artist
 eyeD3 -2 example.mp3 -a id3v2
 
+# Take all the values from v2.4 tag (the default) and set them in the v1 tag.
 eyeD3 -2 --to-v1.1 example.mp3
+# Take all the values from v1 tag and convert to ID3 v2.3
 eyeD3 -1 --to-v2.3 example.mp3
+
+# Remove all the tags
+eyeD3 --remove-all example.mp3
 # [[[endsection]]]
--- docs/plugins/lameinfo_plugin.rst
+++ docs/plugins/lameinfo_plugin.rst
@@ -4,21 +4,4 @@ lameinfo (xing) - Lame (Xing) Header Information
 .. {{{cog
 .. cog.out(cog_pluginHelp("lameinfo"))
 .. }}}
-
-*Outputs lame header (if one exists) for file.*
-
-Names
------
-lameinfo (aliases: xing)
-
-Description
------------
-
-
-Options
--------
-.. code-block:: text
-
-None
-
 .. {{{end}}}
