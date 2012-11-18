503179c86eb216063422f187183c48c8f3ebe91a
nicfit <travis@pobox.com>
2012-11-18 15:11:30 -0700

    merge

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,10 @@ extlinks = {
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.ifconfig',
-              'sphinx.ext.viewcode', 'sphinx.ext.extlinks']
+              'sphinx.ext.viewcode', 'sphinx.ext.extlinks',
+              'sphinxcontrib.bitbucket']
+
+bitbucket_project_url = 'https://bitbucket.org/nicfit/eyed3'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
