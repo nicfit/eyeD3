e058c04e10bcb1decf2c9fe9efb926d75ccdf598
nicfit <travis@pobox.com>
2013-08-04 15:32:39 -0600

    Updated for removal of 'cli'

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -19,8 +19,10 @@
 ################################################################################
 import sys
 from nose.tools import *
-from eyed3.utils import guessMimetype, cli
-from eyed3.utils.cli import printMsg, printWarning, printHeader
+import eyed3.utils.console
+from eyed3.utils import guessMimetype
+from eyed3.utils.console import (printMsg, printWarning, printHeader, Fore,
+                                 WARNING_COLOR, HEADER_COLOR)
 from . import RedirectStdStreams
 
 
@@ -30,19 +32,36 @@ def testId3MimeTypes():
         assert_equal(mt, "application/x-id3")
 
 def test_printWarning():
-    cli.enableColorOutput(sys.stderr, False)
+    eyed3.utils.console.USE_ANSI = False
     with RedirectStdStreams() as out:
         printWarning("Built To Spill")
     assert_equal(out.stderr.read(), "Built To Spill\n")
 
+    eyed3.utils.console.USE_ANSI = True
+    with RedirectStdStreams() as out:
+        printWarning("Built To Spill")
+    assert_equal(out.stderr.read(), "%sBuilt To Spill%s\n" % (WARNING_COLOR(),
+                                                              Fore.RESET))
+
 def test_printMsg():
-    cli.enableColorOutput(sys.stdout, False)
+    eyed3.utils.console.USE_ANSI = False
+    with RedirectStdStreams() as out:
+        printMsg("EYEHATEGOD")
+    assert_equal(out.stdout.read(), "EYEHATEGOD\n")
+
+    eyed3.utils.console.USE_ANSI = True
     with RedirectStdStreams() as out:
         printMsg("EYEHATEGOD")
     assert_equal(out.stdout.read(), "EYEHATEGOD\n")
 
 def test_printHeader():
-    cli.enableColorOutput(sys.stdout, False)
+    eyed3.utils.console.USE_ANSI = False
     with RedirectStdStreams() as out:
         printHeader("Furthur")
     assert_equal(out.stdout.read(), "Furthur\n")
+
+    eyed3.utils.console.USE_ANSI = True
+    with RedirectStdStreams() as out:
+        printHeader("Furthur")
+    assert_equal(out.stdout.read(), "%sFurthur%s\n" % (HEADER_COLOR(),
+                                                       Fore.RESET))
