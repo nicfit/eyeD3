350efcba58c1e8a3a368faeb1ef7e2e49715f6f4
Travis Shirk <travis@pobox.com>
2013-11-30 17:43:36 -0700

    Added generated info.py to be able to run from checkout in pip. Don't like.

--- pavement.py
+++ pavement.py
@@ -33,7 +33,7 @@ except:
     paverutils = None
 
 PROJECT = u"eyeD3"
-VERSION = "0.7.5"
+VERSION = "0.7.5-beta"
 
 LICENSE = open("COPYING", "r").read().strip('\n')
 DESCRIPTION = "Python audio data toolkit (ID3 and MP3)"
