0ac928fc453e08cd7eb60e3956188dd9ee4ac07e
mafro <github@mafro.net>
2014-03-29 12:47:30 +1100

    allow user to specific naming templates in fixup plugin

--- src/eyed3/plugins/fixup.py
+++ src/eyed3/plugins/fixup.py
@@ -28,7 +28,7 @@ from eyed3.plugins import LoaderPlugin
 from eyed3.utils.prompt import prompt
 from eyed3.utils.console import printMsg, printError, Style, Fore, Back
 from eyed3 import LOCAL_ENCODING
-from eyed3 import core
+from eyed3 import core, id3
 
 from eyed3.core import (ALBUM_TYPE_IDS, TXXX_ALBUM_TYPE,
                         LP_TYPE, EP_TYPE, COMP_TYPE, VARIOUS_TYPE, DEMO_TYPE,
@@ -88,9 +88,11 @@ The following test and fixes always apply:
         - Type ``various``: %(VARIOUS_FNAME_FORMAT)s
         - Type ``single``: %(SINGLE_FNAME_FORMAT)s
         - All other types: %(NORMAL_FNAME_FORMAT)s
+        - A rename template can be supplied in --file-rename-pattern
     12. Directories are renamed as follows:
         - Type ``live``: %(LIVE_DNAME_FORMAT)s
         - All other types: %(NORMAL_DNAME_FORMAT)s
+        - A rename template can be supplied in --dir-rename-pattern
 
 Album types:
 
@@ -128,6 +130,10 @@ Album types:
                        help=ARGS_HELP["--no-prompt"])
         g.add_argument("--dotted-dates", action="store_true",
                        help=ARGS_HELP["--dotted-dates"])
+        g.add_argument("--file-rename-pattern", dest="file_rename_pattern",
+                       help=ARGS_HELP["--file-rename-pattern"])
+        g.add_argument("--dir-rename-pattern", dest="dir_rename_pattern",
+                       help=ARGS_HELP["--dir-rename-pattern"])
 
     def _getOne(self, key, values, default=None, Type=unicode, required=True):
         values = set(values)
@@ -455,12 +461,15 @@ Album types:
 
         # File renaming
         file_renames = []
-        if dir_type == SINGLE_TYPE:
-            format_str = SINGLE_FNAME_FORMAT
-        elif dir_type == VARIOUS_TYPE:
-            format_str = VARIOUS_FNAME_FORMAT
+        if self.args.file_rename_pattern:
+            format_str = self.args.file_rename_pattern
         else:
-            format_str = NORMAL_FNAME_FORMAT
+            if dir_type == SINGLE_TYPE:
+                format_str = SINGLE_FNAME_FORMAT
+            elif dir_type == VARIOUS_TYPE:
+                format_str = VARIOUS_FNAME_FORMAT
+            else:
+                format_str = NORMAL_FNAME_FORMAT
 
         for f in audio_files:
             orig_name, orig_ext = os.path.splitext(os.path.basename(f.path))
@@ -472,18 +481,21 @@ Album types:
         # Directory renaming
         dir_rename = None
         if dir_type != SINGLE_TYPE:
-            if dir_type == LIVE_TYPE:
-                dir_format = LIVE_DNAME_FORMAT
+            if self.args.dir_rename_pattern:
+                dir_format = self.args.dir_rename_pattern
             else:
-                dir_format = NORMAL_DNAME_FORMAT
-            template = TagTemplate(dir_format,
-                                   dotted_dates=self.args.dotted_dates)
+                if dir_type == LIVE_TYPE:
+                    dir_format = LIVE_DNAME_FORMAT
+                else:
+                    dir_format = NORMAL_DNAME_FORMAT
+                template = TagTemplate(dir_format,
+                                       dotted_dates=self.args.dotted_dates)
 
-            pref_dir = template.substitute(audio_files[0].tag, zeropad=True)
-            if os.path.basename(directory) != pref_dir:
-                new_dir = os.path.join(os.path.dirname(directory), pref_dir)
-                printMsg("Rename directory to %s" % new_dir)
-                dir_rename = (directory, new_dir)
+                pref_dir = template.substitute(audio_files[0].tag, zeropad=True)
+                if os.path.basename(directory) != pref_dir:
+                    new_dir = os.path.join(os.path.dirname(directory), pref_dir)
+                    printMsg("Rename directory to %s" % new_dir)
+                    dir_rename = (directory, new_dir)
 
         if not self.args.dry_run:
             confirmed = False
@@ -509,6 +521,12 @@ Album types:
             printMsg("\nNo changes made (run without -n/--dry-run)")
 
 
+def _getTemplateKeys():
+    keys = list(id3.TagTemplate("")._makeMapping(None, False).keys())
+    keys.sort()
+    return ", ".join(["$%s" % v for v in keys])
+
+
 ARGS_HELP = {
         "--type": "How to treat each directory. The default is '%s', "
                   "although you may be prompted for an alternate choice "
@@ -520,4 +538,10 @@ ARGS_HELP = {
         "--no-prompt": "Exit if prompted.",
         "--dotted-dates": "Separate date with '.' instead of '-' when naming "
                           "directories.",
+        "--file-rename-pattern": "Rename file (the extension is not affected) "
+                                 "based on data in the tag using substitution "
+                                 "variables: " + _getTemplateKeys(),
+        "--dir-rename-pattern": "Rename directory based on data in the tag "
+                                "using substitution variables: " +
+                                _getTemplateKeys(),
 }
