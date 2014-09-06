648d9c1db7b9e4e791905945f8907e94dbe5cb93
nicfit <travis@pobox.com>
2014-09-05 23:29:15 -0600

    merge release 0.7.5

---
+++ etc/eyeD3.bash
@@ -0,0 +1,30 @@
+# bash-completion for 'eyeD3'
+
+unset __EYED3_COMPLETION_LONG_OPT
+
+function _eyeD3_completion()
+{
+    [[ -e `which eyeD3 2> /dev/null` ]] || return 0
+
+    # Variables to hold the current word and possible matches
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local opts=()
+
+    case "${cur}" in
+        -*)
+            if [[ -z "${__EYED3_COMPLETION_LONG_OPT}" ]]; then
+                export __EYED3_COMPLETION_LONG_OPT=$(
+                    eyeD3 --help | egrep -o " \-[A-Za-z0-9_\.\-]+\=?" | sort -u)
+            fi
+            opts="${__EYED3_COMPLETION_LONG_OPT}"
+            ;;
+
+        *)
+            ;;
+    esac
+
+    # Set possible completions
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+}
+
+complete -o default -o nospace  -F _eyeD3_completion eyeD3
---
+++ examples/cli_examples.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+alias eyeD3='eyeD3 --no-color --no-config'
+
+# [[[section SETUP]]]
+rm -f example.id3
+touch example.id3
+ls -o example.id3
+# [[[endsection]]]
+
+# [[[section ART_TIT_SET]]]
+eyeD3 --artist="Token Entry" --title="Entities" example.id3 -Q
+# [[[endsection]]]
+
+# [[[section ALB_YR_G_SET]]]
+eyeD3 -A "Jaybird" -Y 1987 -G "Hardcore" example.id3 -Q
+eyeD3 example.id3
+# [[[endsection]]]
+
+# [[[section NONSTD_GENRE_SET]]]
+eyeD3 --genre="New York City Hardcore" example.id3 -Q
+eyeD3 example.id3
+# [[[endsection]]]
+
+# [[[section CONVERT1]]]
+# Convert the current v2.4 frame to v2.3
+eyeD3 --to-v2.3 example.id3 -Q
+# Convert back
+eyeD3 --to-v2.4 example.id3 -Q
+# Convert to v1, this will lose all the more advanced data members ID3 v2 offers
+eyeD3 --to-v1.1 example.id3 -Q
+# [[[endsection]]]
+
+# [[[section DISPLAY_V1]]]
+eyeD3 -1 example.id3
+# [[[endsection]]]
+
+# [[[section SET_WITH_VERSIONS]]]
+# Set an artist value in the ID3 v1 tag
+eyeD3 -1 example.id3 -a id3v1
+# The file now has a v1 and v2 tag, change the v2 artist
+eyeD3 -2 example.id3 -a id3v2
+# Take all the values from v2.4 tag (the default) and set them in the v1 tag.
+eyeD3 -2 --to-v1.1 example.id3
+# Take all the values from v1 tag and convert to ID3 v2.3
+eyeD3 -1 --to-v2.3 example.id3
+# [[[endsection]]]
+
+# [[[section IMG_URL]]]
+eyeD3 --add-image http\\://example.com/cover.jpg:FRONT_COVER example.id3
+# [[[endsection]]]
+
+# [[[section GENRES_PLUGIN1]]]
+eyeD3 --plugin=genres
+# [[[endsection]]]
+
+# [[[section LAME_PLUGIN]]]
+eyeD3 -P lameinfo src/test/data/notag-vbr.mp3
+# [[[endsection]]]
+
+# [[[section PLUGINS_LIST]]]
+eyeD3 --plugins
+# [[[endsection]]]
+
+# [[[section ITUNES_PODCAST_PLUGIN]]]
+eyeD3 -P itunes-podcast example.id3
+eyeD3 -P itunes-podcast example.id3 --add
+eyeD3 -P itunes-podcast example.id3 --remove
+# [[[endsection]]]
+
+# [[[section REMOVE_ALL_TAGS]]]
+eyeD3 --remove-all example.id3
+# [[[endsection]]]
