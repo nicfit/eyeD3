db845e12f453d36b169607d2f1019136b700d38a
Travis Shirk <travis@pobox.com>
2014-09-28 09:19:17 -0600

    Added test data tasks.

--- pavement.py
+++ pavement.py
@@ -574,3 +574,26 @@ def run2to3(options):
 
     sh("%(cmd)s %(common_opts)s %(cmd_opts)s %(paths)s >| %(cmd)s.patch" %
        locals())
+
+
+TEST_DATA_FILE = "eyeD3-test-data.tgz"
+TEST_DATA_D = os.path.splitext(TEST_DATA_FILE)[0]
+
+@task
+def test_data(options):
+    cwd = os.getcwd()
+
+    sh("wget 'http://nicfit.net/files/%(TEST_DATA_FILE)s'" % globals())
+    sh("tar xzf ./%(TEST_DATA_FILE)s -C ./src/test" % globals())
+    try:
+        os.chdir("./src/test")
+        sh("ln -s ./%(TEST_DATA_D)s ./data" % globals())
+    finally:
+        os.chdir(cwd)
+
+@task
+def test_data_clean(options):
+    sh("rm ./%(TEST_DATA_FILE)s" % globals())
+    if os.path.lexists("src/test/data"):
+        sh("rm src/test/data")
+    sh("rm -rf src/test/%(TEST_DATA_D)s" % globals(), ignore_error=True)
