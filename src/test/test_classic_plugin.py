0925e9e28a1d82e89a0a62b741a15f92c60cb3ca
Travis Shirk <travis@pobox.com>
2017-02-02 23:02:20 -0700

    Removed header !minor

--- src/test/test_classic_plugin.py
+++ src/test/test_classic_plugin.py
@@ -1,21 +1,4 @@
 # -*- coding: utf-8 -*-
-################################################################################
-#  Copyright (C) 2012  Travis Shirk <travis@pobox.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, see <http://www.gnu.org/licenses/>.
-#
-################################################################################
 import sys
 if sys.version_info[:2] == (2, 6):
     import unittest2 as unittest
