167f45c84cf3cd355ea3aa639d561fd73256b30f
Travis Shirk <travis@pobox.com>
2017-03-26 11:28:57 -0600

    fix: bytes vs unicode

--- src/eyed3/id3/frames.py
+++ src/eyed3/id3/frames.py
@@ -1,21 +1,4 @@
 # -*- coding: utf-8 -*-
-################################################################################
-#  Copyright (C) 2012-2015  Travis Shirk <travis@pobox.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, see <http://www.gnu.org/licenses/>.
-#
-################################################################################
 from io import BytesIO
 from codecs import ascii_encode
 from collections import namedtuple
@@ -729,10 +712,9 @@ class ImageFrame(Frame):
         name = ImageFrame.picTypeToString(self.picture_type) if not name \
                                                              else name
         ext = self.mime_type.split("/")[1]
-        if ext == b"jpeg":
-            ext = b"jpg"
-        name = '.'.join([name, ext.decode('latin1')])
-        return name
+        if ext == "jpeg":
+            ext = "jpg"
+        return ".".join([name, ext])
 
 
 class ObjectFrame(Frame):
