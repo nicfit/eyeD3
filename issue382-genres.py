798b6a8accc5155ae304e068b7b151dff459292d
Travis Shirk <travis@pobox.com>
2020-02-14 20:19:08 -0700

    .

---
+++ issue382-genres.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+import eyed3
+
+meta = eyed3.load('sample.mp3')
+if len(sys.argv) > 1:
+    #meta.tag.genre = sys.argv[1]
+    meta.tag.non_std_genre = sys.argv[1]
+    meta.tag.save()
+
+print("Genre:", meta.tag.genre)
+print("Non std genre:", meta.tag.non_std_genre)
