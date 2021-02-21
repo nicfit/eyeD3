c8b5f300cdb2eb6df58a251d9d96f0038d2ed97b
Travis Shirk <travis@pobox.com>
2021-02-20 17:10:20 -0700

    chg: dev: requests version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='eyeD3',
-    version='0.9.6',
+    version='0.9.7b',
     description='Python audio data toolkit (ID3 and MP3)',
     python_requires='==3.*,>=3.6.0',
     author='Travis Shirk',
@@ -34,9 +34,9 @@ setup(
     classifiers=['Environment :: Console', 'Intended Audience :: End Users/Desktop', 'Topic :: Multimedia :: Sound/Audio :: Editors', 'Topic :: Software Development :: Libraries :: Python Modules', 'Intended Audience :: Developers', 'Operating System :: POSIX', 'Natural Language :: English', 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 'Programming Language :: Python', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: 3.9'],
     entry_points={"console_scripts": ["eyeD3 = eyed3.main:_main"]},
     packages=['eyed3', 'eyed3.id3', 'eyed3.mp3', 'eyed3.plugins', 'eyed3.utils'],
-    package_dir={"": "."},
+    package_dir={"": "src"},
     package_data={"eyed3.plugins": ["*.ebnf"]},
     install_requires=['coverage[toml]==5.*,>=5.3.1', 'dataclasses==0.*,>=0.8.0; python_version == "3.6.*" and python_version >= "3.6.0"', 'deprecation==2.*,>=2.1.0', 'filetype==1.*,>=1.0.7'],
     dependency_links=['git+https://github.com/nicfit/gitchangelog.git@nicfit.py#egg=gitchangelog'],
-    extras_require={"art-plugin": ["pillow==8.*,>=8.0.1", "pylast==4.*,>=4.0.0", "requests==2.*,>=2.25.0"], "dev": ["cogapp==3.*,>=3.0.0", "gitchangelog", "nicfit.py[cookiecutter]==0.*,>=0.8.7", "paver==1.*,>=1.3.4", "regarding==0.*,>=0.1.4", "sphinx==3.*,>=3.4.1", "sphinx-issues==1.*,>=1.2.0", "sphinx-rtd-theme==0.*,>=0.5.0", "twine==3.*,>=3.3.0", "wheel==0.*,>=0.36.2"], "display-plugin": ["grako==3.*,>=3.99.9"], "test": ["check-manifest==0.*,>=0.45.0", "factory-boy==3.*,>=3.1.0", "flake8==3.*,>=3.8.4", "pytest==6.*,>=6.2.1", "pytest-cov==2.*,>=2.10.1", "tox==3.*,>=3.20.1"], "yaml-plugin": ["ruamel.yaml==0.*,>=0.16.12"]},
+    extras_require={"art-plugin": ["pillow==8.*,>=8.0.1", "pylast==4.*,>=4.0.0", "requests==2.*,>=2.25.0"], "dev": ["cogapp==3.*,>=3.0.0", "dephell==0.*,>=0.8.3", "gitchangelog", "nicfit.py[cookiecutter]==0.*,>=0.8.7", "paver==1.*,>=1.3.4", "regarding==0.*,>=0.1.4", "sphinx==3.*,>=3.4.1", "sphinx-issues==1.*,>=1.2.0", "sphinx-rtd-theme==0.*,>=0.5.0", "twine==3.*,>=3.3.0", "wheel==0.*,>=0.36.2"], "display-plugin": ["grako==3.*,>=3.99.9"], "test": ["check-manifest==0.*,>=0.45.0", "factory-boy==3.*,>=3.1.0", "flake8==3.*,>=3.8.4", "pytest==6.*,>=6.2.1", "pytest-cov==2.*,>=2.10.1", "tox==3.*,>=3.20.1"], "yaml-plugin": ["ruamel.yaml==0.*,>=0.16.12"]},
 )
