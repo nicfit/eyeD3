0ad57cf49809af9cc3ea812b47886a7560803c9b
Daniel Musketa <daniel@musketa.de>
2026-02-13 00:13:02 +0100

    spell correct self.description in ImageFrame.parse()
    in case it makes sense to keep it at all

--- eyed3/id3/frames.py
+++ eyed3/id3/frames.py
@@ -635,7 +635,7 @@ class ImageFrame(Frame):
             self.picture_type = pt
         log.debug("APIC picture type: %d" % self.picture_type)
 
-        self.desciption = ""
+        self.description = ""
 
         # Remaining data is a NULL separated description and image data
         buffer = input.read()
