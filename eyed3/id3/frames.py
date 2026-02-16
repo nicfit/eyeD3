b97d708e261e4ac38e1e0006bb3e37b455d44a1e
Daniel Musketa <daniel@musketa.de>
2026-02-16 19:17:03 +0100

    fix image frame description (#692)
    `description` is spelled wrong and gets set a few lines later anyways.
    So we could either keep it like this:
    
    
    https://github.com/vlt/eyeD3/blob/0ad57cf49809af9cc3ea812b47886a7560803c9b/eyed3/id3/frames.py#L637-L648
    
    or delete it ;-)
    
    ---------
    
    Co-authored-by: nicfit <travis@pobox.com>

--- eyed3/id3/frames.py
+++ eyed3/id3/frames.py
@@ -636,8 +636,6 @@ class ImageFrame(Frame):
             self.picture_type = pt
         log.debug("APIC picture type: %d" % self.picture_type)
 
-        self.desciption = ""
-
         # Remaining data is a NULL separated description and image data
         buffer = input.read()
         input.close()
